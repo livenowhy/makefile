2ac281926e4d2952bacd1585bf8fcf1a95bf672b
livenowhy <livenowhy@126.com>
2025-05-21 10:44:53 +0800

    ...

---
+++ 13_django/requirements/bidui.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+@author: livenowhy
+@contact: liuzhangpei@126.com
+"""
+
+
+import os
+
+
+if __name__ == "__main__":
+
+    # 1. 获取 Dockerfile 版本
+    with open('/Users/zpliu/Desktop/share/makefile/13_django/requirements/dockerfile.txt') as fp:
+        dockerfile_lines = [line.replace('\n', '').upper() for line in fp.readlines()]
+        
+    # 1. 获取 镜像中 pip 版本
+    with open('/Users/zpliu/Desktop/share/makefile/13_django/requirements/image.txt') as fp:
+        image_lines = [line.replace('\n', '').upper() for line in fp.readlines()]
+
+    
+    print(set(dockerfile_lines) - set(image_lines))
+
+    # print(set(dockerfile_lines) - set(image_lines))
